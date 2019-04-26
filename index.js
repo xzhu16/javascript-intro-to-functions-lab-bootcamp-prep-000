082f523092741f6c8d8c2d7393dc5bcca5ed9cb5
Xi Zhu <zxjucy2012@gmail.com>
2019-04-26 07:56:48 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  
+  return string
+}
